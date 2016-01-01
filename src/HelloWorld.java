6815434596c722628763ebeed244d151cdd6885b
justinliow <justinliow@gmail.com>
2016-01-01 16:18:33 -0600

    Adding Day 0 and 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class HelloWorld {
+
+       public static void main(String[] args) {
+            System.out.println("Hello World.");
+            System.out.println("Welcome to 30 Days of Code.");
+          // Write your print statements here!! 
+          // Template: System.out.println("what you want to print");
+        
+       }
+}
