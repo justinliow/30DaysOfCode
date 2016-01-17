d2d313e0224ad88d78bb7363bc1628ca0e3d28dc
justinliow <justinliow@gmail.com>
2016-01-16 20:13:08 -0500

    ...

--- src/Day10Binary.java
+++ src/Day10Binary.java
@@ -1,4 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Day10Binary {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int j=0; j<T; j++)
+        {
+            sc.nextLine();
+            double n=sc.nextDouble();
+            double nsave=n;
+            int npow=31;
+            while(Math.pow(2,npow)>n)
+            {
+                npow--;
+            }
+             // System.out.println(n);
+             // System.out.println(Math.pow(2, npow));
+            int npowc=npow;
+            for(int i=0; i<npowc; i++)
+            {
+                if(n>=Math.pow(2,npow))
+                {
+                //System.out.println(n);
+               // System.out.println(Math.pow(2, npow));
+                n=n-Math.pow(2,npow);
+                npow--;
+                System.out.print("1");
+                }
+                else
+                {
+                    npow--;
+                    System.out.print("0");
+                }
+            }
+            if(nsave%2==0)
+            {
+                System.out.print("0");    
+            }
+            else
+                System.out.print("1");
+            System.out.println("");
+        }
+    }
 }
--- src/Day9Recursion.java
+++ src/Day9Recursion.java
@@ -1,4 +1,20 @@
-
+import java.io.*;
+import java.util.*;
 public class Day9Recursion {
-
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        //Take Input
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+        int gcd=find_gcd(a,b);
+        System.out.println(gcd);
+    }
+   static  int find_gcd(int a,int b){
+       if(a==b)
+              return a;
+       else 
+              return find_gcd(Math.max(a,b)-Math.min(a,b),Math.min(a,b));
+    }
 }
+ 
